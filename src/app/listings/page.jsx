12a9d61637964bc2edfa25d224e6d44b35b0c6d2
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-28 14:44:57 +0700

    Update page: Listings

--- src/app/listings/page.jsx
+++ src/app/listings/page.jsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import { defineOneEntry } from 'oneentry'
 
-export default function Listings() {
-	return <div>Listings</div>
+import ListingItem from '../_components/ListingItem'
+
+const { Products } = defineOneEntry('https://smartestate.oneentry.cloud', {
+	token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+})
+
+export default async function Listings() {
+	const allListings = await Products.getProductsByPageUrl('listings')
+
+	return (
+		<div>
+			<h1 className='text-3xl font-semibold text-slate-600 text-center my-10'>
+				Recent listings
+			</h1>
+
+			<div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+				{allListings && allListings.length > 0 && (
+					<div className='flex flex-wrap gap-4'>
+						{allListings.map((listing) => (
+							<ListingItem listing={listing} key={listing.id} />
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
