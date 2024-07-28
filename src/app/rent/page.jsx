74db68e6f45c0dcd073281dc5889f522313f7def
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-28 14:51:13 +0700

    Update page: Rent

--- src/app/rent/page.jsx
+++ src/app/rent/page.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import { defineOneEntry } from 'oneentry'
 
-export default function Rent() {
-	return <div>Rent</div>
+import ListingItem from '../_components/ListingItem'
+
+const { Products } = defineOneEntry('https://smartestate.oneentry.cloud', {
+	token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+})
+
+export default async function Rent() {
+	const rentListings = await Products.getProductsByPageUrl('rent')
+
+	return (
+		<div>
+			<h1 className='text-3xl font-semibold text-slate-600 text-center my-10'>
+				Recent places for rent
+			</h1>
+			<div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+				{rentListings && rentListings.length > 0 && (
+					<div className='flex flex-wrap gap-4'>
+						{rentListings.map((listing) => (
+							<ListingItem listing={listing} key={listing.id} />
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
