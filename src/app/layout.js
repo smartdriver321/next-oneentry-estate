ce9c3e09435cd729123fda5113befbe96ce7e564
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-28 11:27:47 +0700

    Create pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-	title: 'smart-estate.com',
+	title: 'Smart Estate Property',
 	description: 'Rent & Sale your Property',
 }
 
